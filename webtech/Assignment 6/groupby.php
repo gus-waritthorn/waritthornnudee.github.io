ff2364d44a4e622db3b8909921a31e609af5557b
waritthornnudee <waritthorn.v@ku.th>
2017-05-14 20:19:15 +0700

    Add files via upload

--- webtech/Assignment 6/groupby.php
+++ webtech/Assignment 6/groupby.php
@@ -1,56 +1,53 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
+<!DOCTYPE html>
+<html>
 <body>
+
 <?php
 echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+  echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator { 
-    function __construct($it) { 
-        parent::__construct($it, self::LEAVES_ONLY); 
-    }
+     function __construct($it) { 
+         parent::__construct($it, self::LEAVES_ONLY); 
+     }
 
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
+     function current() {
+         return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+     }
 
-    function beginChildren() { 
-        echo "<tr>"; 
-    } 
+     function beginChildren() { 
+         echo "<tr>"; 
+     } 
 
-    function endChildren() { 
-        echo "</tr>" . "\n";
-    } 
+     function endChildren() { 
+         echo "</tr>" . "\n";
+     } 
 } 
 
 $servername = "localhost";
-$username = "noo";
-$password = "mypass";
+$username = "root";
+$password = "";
 $dbname = "dreamhome";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM staff WHERE sex='F'"); 
-    $stmt->execute();
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $stmt = $conn->prepare("SELECT * FROM staff"); 
+     $stmt->execute();
 
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-        echo $v;
-    }
+     // set the resulting array to associative
+     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+         echo $v;
+     }
 }
 catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
+     echo "Error: " . $e->getMessage();
 }
 $conn = null;
 echo "</table>";
-?>
-<input type="submit" name"submit" value="submit" />
+?>  
+
 </body>
 </html>
--- webtech/Assignment 6/insert_sql.php
+++ webtech/Assignment 6/insert_sql.php
@@ -4,20 +4,20 @@
 <?php
 if(isset($_POST['submit'])){
 	$servername = "localhost";
-	$username = "noo";
-	$password = "mypass";
-	$dbname = "webtech";
-	$customerval = $_POST['customerid'];
-	$idval = $_POST['id'];
-	$firstval = $_POST['firstname'];
-	$lastval = $_POST['lastname'];
+	$username = "root";
+	$password = "";
+	$dbname = "dreamhome";
+	$customernoval = $_POST['customerno'];
+	$fnameval = $_POST['firstname'];
+	$lnameval = $_POST['lastname'];
+	$telnoval = $_POST['telnumber'];
 	
 	try {
 		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 		// set the PDO error mode to exception
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO customerid (CustomerID, CitizenID, Firstname, Lastname)
-		VALUES ('$customerval', '$idval', '$firstval', '$lastval')";
+		$sql = "INSERT INTO client (clientno, fname, lname, telno)
+		VALUES ('$customernoval', '$fnameval', '$lnameval', '$telnoval')";
 		// use exec() because no results are returned
 		$conn->exec($sql);
 		echo "New record created successfully";
@@ -31,10 +31,10 @@ if(isset($_POST['submit'])){
 }
 ?>
 <form method="post">
-CustomerID <input type="text" name="customerid" /><br><br>
-CitenzenID <input type="text" name="id" /><br><br>
-FistName <input type="text" name="firstname" /><br><br>
+CustomerNo <input type="text" name="customerno" /><br><br>
+FirstName <input type="text" name="firstname" /><br><br>
 LastName <input type="text" name="lastname" /><br><br>
+TelNo <input type="text" name="telnumber" /><br><br>
 <input type="submit" name="submit" value="submit" />
 </form>
 </body>
